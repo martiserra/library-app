7b0d1cb187272228e7b21fae9f761ead9c35d371
martiserra <marti.serra.alabau@gmail.com>
2013-12-30 16:12:38 +0100

    added mock data in libraries controller

--- controllers/libraries.js
+++ controllers/libraries.js
@@ -2,6 +2,12 @@
  * Controller for libraries routes
  */
 
+var libraries = [
+{"id":1, "name":"Demo Biblioteca", "places":120, "available":"1/1/2013"},
+{"id":2, "name":"Ciències UAB", "places":220, "available":"5/5/2014"},
+{"id":3, "name":"More Coming Soon", "places":0, "available":"1/1/2015"},
+];
+
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.send(libraries);
 };
